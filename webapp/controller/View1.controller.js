78d3ae160991559807be67802fcde88c57e9eacf
Patrick Pang <patrick.pang@vu.edu.au>
2021-03-04 13:22:46 +0000

    Changed JS object with code and updating the model. Added a tab bar.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,6 +10,14 @@ sap.ui.define([
 		return Controller.extend("sap.btp.sapui5.controller.View1", {
 			onInit: function () {
 
-			}
+            },
+            
+            handleListPress: function (evt) {
+                var list = evt.getSource().getParent();
+                var flight = evt.getSource().getBindingContext().getObject();
+                debugger;
+                flight.passengers += 10;
+                list.getModel().refresh(true);
+            }
 		});
 	});
